641e80199f19100dd8b9a8ec45b39ded6761436f
shazhko <shazhko.artem@nixsolutions.com>
2021-07-21 23:01:16 +0300

    move User class to the new file and uncomment code

--- EventSource.Client.Functions/Functions/HandleEventFunction.cs
+++ EventSource.Client.Functions/Functions/HandleEventFunction.cs
@@ -1,5 +1,6 @@
 using Azure.WebJobs.Extensions.EventSource.Attributes;
 using EventSource.Client.Functions.Constants;
+using EventSource.Client.Functions.Models;
 using EventSource.Client.Functions.Services;
 using EventSource.Common.Models.Messages;
 using Microsoft.Azure.WebJobs;
@@ -7,12 +8,6 @@ using Microsoft.Extensions.Logging;
 
 namespace EventSource.Client.Functions.Functions
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-    }
-
     public class HandleEventFunction
     {
         private readonly IStore<EventMessage> store;
@@ -22,7 +17,7 @@ namespace EventSource.Client.Functions.Functions
             this.store = store;
         }
 
-        [FunctionName("HandleCreatedUserEventFunction")]
+        [FunctionName(nameof(HandleCreatedUserEventFunction))]
         public void HandleCreatedUserEventFunction(
             [EventSourceTrigger(ConfigurationSectionNames.EventSource.EventNames.CreatedUser, ConfigurationSectionNames.EventSource.Connection)] string message, ILogger log)
         {
@@ -30,44 +25,44 @@ namespace EventSource.Client.Functions.Functions
             this.store.Add(new StringEventMessage(ConfigurationSectionNames.EventSource.EventNames.CreatedUser, message));
         }
 
-        //[FunctionName("HandleUpdatedUserEventFunction")]
-        //public void HandleUpdatedUserEventFunction(
-        //    [EventSourceTrigger(ConfigurationSectionNames.EventSource.EventNames.UpdatedUser, ConfigurationSectionNames.EventSource.Connection)] User message, ILogger log)
-        //{
-        //    log.LogInformation($"C#  EventSource trigger function processed message: {message.UserName}");
-        //    this.store.Add(new CustomEventMessage<User>(ConfigurationSectionNames.EventSource.EventNames.UpdatedUser, message));
-        //}
+        [FunctionName(nameof(HandleUpdatedUserEventFunction))]
+        public void HandleUpdatedUserEventFunction(
+            [EventSourceTrigger(ConfigurationSectionNames.EventSource.EventNames.UpdatedUser, ConfigurationSectionNames.EventSource.Connection)] User message, ILogger log)
+        {
+            log.LogInformation($"C#  EventSource trigger function processed message: {message.UserName}");
+            this.store.Add(new CustomEventMessage<User>(ConfigurationSectionNames.EventSource.EventNames.UpdatedUser, message));
+        }
 
-        //[FunctionName("HandleDeletedUserEventFunction")]
-        //public void HandleDeletedUserEventFunction(
-        //    [EventSourceTrigger("DeletedUser", ConfigurationSectionNames.EventSource.Connection)] CustomEventMessage<User> message, ILogger log)
-        //{
-        //    log.LogInformation($"C#  EventSource trigger function processed message: {message.Content.UserName}");
-        //    this.store.Add(message);
-        //}
+        [FunctionName(nameof(HandleDeletedUserEventFunction))]
+        public void HandleDeletedUserEventFunction(
+            [EventSourceTrigger("DeletedUser", ConfigurationSectionNames.EventSource.Connection)] CustomEventMessage<User> message, ILogger log)
+        {
+            log.LogInformation($"C#  EventSource trigger function processed message: {message.Content.UserName}");
+            this.store.Add(message);
+        }
 
-        //[FunctionName("HandleCreatingUserErrorEventFunction")]
-        //public void HandleCreatingUserErrorEventFunction(
-        //    [EventSourceTrigger("CreatingUserError", ConfigurationSectionNames.EventSource.Connection)] EventMessage message, ILogger log)
-        //{
-        //    log.LogInformation($"C#  EventSource trigger function processed message: {message.Name}");
-        //    this.store.Add(message);
-        //}
+        [FunctionName(nameof(HandleCreatingUserErrorEventFunction))]
+        public void HandleCreatingUserErrorEventFunction(
+            [EventSourceTrigger("CreatingUserError", ConfigurationSectionNames.EventSource.Connection)] EventMessage message, ILogger log)
+        {
+            log.LogInformation($"C#  EventSource trigger function processed message: {message.Name}");
+            this.store.Add(message);
+        }
 
-        //[FunctionName("HandleUpdatingUserErrorEventFunction")]
-        //public void HandleUpdatingUserErrorEventFunction(
-        //    [EventSourceTrigger("UpdatingUserError", ConfigurationSectionNames.EventSource.Connection)] StringEventMessage message, ILogger log)
-        //{
-        //    log.LogInformation($"C#  EventSource trigger function processed message: {message.Name}");
-        //    this.store.Add(message);
-        //}
+        [FunctionName(nameof(HandleUpdatingUserErrorEventFunction))]
+        public void HandleUpdatingUserErrorEventFunction(
+            [EventSourceTrigger("UpdatingUserError", ConfigurationSectionNames.EventSource.Connection)] StringEventMessage message, ILogger log)
+        {
+            log.LogInformation($"C#  EventSource trigger function processed message: {message.Name}");
+            this.store.Add(message);
+        }
 
-        //[FunctionName("HandleDeletingUserErrorEventFunction")]
-        //public void HandleDeletingUserErrorEventFunction(
-        //    [EventSourceTrigger("DeletingUserError", ConfigurationSectionNames.EventSource.Connection)] BytesEventMessage message, ILogger log)
-        //{
-        //    log.LogInformation($"C#  EventSource trigger function processed message: {message.Name}");
-        //    this.store.Add(message);
-        //}
+        [FunctionName(nameof(HandleDeletingUserErrorEventFunction))]
+        public void HandleDeletingUserErrorEventFunction(
+            [EventSourceTrigger("DeletingUserError", ConfigurationSectionNames.EventSource.Connection)] BytesEventMessage message, ILogger log)
+        {
+            log.LogInformation($"C#  EventSource trigger function processed message: {message.Name}");
+            this.store.Add(message);
+        }
     }
 }
