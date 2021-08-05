d28e77269f1b7594d6b399650faa7e7386e3ea6c
shazhko <shazhko.artem@nixsolutions.com>
2021-08-05 22:35:21 +0300

    use EventSourceTrigger instead of the HttpTrigger in the GenerateReport

--- EventSource.Client.Functions/Functions/ActivityFunctions.cs
+++ EventSource.Client.Functions/Functions/ActivityFunctions.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Threading.Tasks;
-using Azure.WebJobs.Extensions.EventSource.Attributes;
+﻿using Azure.WebJobs.Extensions.EventSource.Attributes;
 using EventSource.Client.Functions.Constants;
 using EventSource.Client.Functions.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+using System;
 
 namespace EventSource.Client.Functions.Functions
 {
     public class ActivityFunctions
     {
         [FunctionName(nameof(GenerateReport))]
-        public async Task<IActionResult> GenerateReport(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] Activity activity,
+        public async void GenerateReport(
+            [EventSourceTrigger("newActivity", ConfigurationSectionNames.EventSource.Connection)] Activity activity,
             [EventSource(eventName: "newReport", ConfigurationSectionNames.EventSource.Connection)] IAsyncCollector<Report> reportCollector)
         {
             var report = new Report { Name = activity.Name, Date = DateTime.Now };
             await reportCollector.AddAsync(report);
-            return new OkResult();
         }
     }
 }
